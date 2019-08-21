ea44a3e9a287f9e9bcec7095a6d982db6bb0a1b8
kxp790 <kxp790@student.bham.ac.uk>
2019-08-21 15:36:35 +0100

    add, get emp id

--- tractorz-backend/db.js
+++ tractorz-backend/db.js
@@ -12,7 +12,7 @@ db.connect(function(err) {
     if (err) throw err;
     console.log("Connected to MySQL");
   });
-  
+
 exports.getEmployees = function(callback) {
     db.query("SELECT *"
     + " FROM employee",
@@ -20,4 +20,22 @@ exports.getEmployees = function(callback) {
       if (err) throw err;
       callback(rows);
     });
+};
+
+exports.getEmployeeId = function(id, callback) {
+    db.query("SELECT id"
+    + " FROM employee WHERE employee_id = ?",
+    [id],
+    function(err, rows) {
+      if (err) throw err;
+      callback(rows);
+    });
+  }
+
+exports.addEmployee = function(data, readyFn) {
+    db.query("INSERT INTO employee SET ?", data,
+      function(error, results, fields) {
+          if (error) throw error;
+          readyFn(results);
+      });
 };
