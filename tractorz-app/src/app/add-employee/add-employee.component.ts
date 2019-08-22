056a312c912afccbaff2512b8a2e5ea207e9db27
Jana <janal@kainos.com>
2019-08-22 11:52:31 +0100

    add addEmployee Function and Css

---
+++ tractorz-app/src/app/add-employee/add-employee.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { DataService } from '../data.service';
+
+
+@Component({
+  selector: 'tractorz-add-employee',
+  templateUrl: './add-employee.component.html',
+  styleUrls: ['./add-employee.component.css']
+})
+export class AddEmployeeComponent implements OnInit {
+  public newEmployee: Employee;
+  data: DataService;
+
+  constructor(dataService: DataService) {
+    this.data = dataService;
+  }
+
+  addEmployee(addForm): void {
+    console.log("addEmployee");
+    if (addForm.valid) {
+      this.data.addEmployee(this.newEmployee).then(employee => {
+        console.log(employee);
+        this.newEmployee = new Employee();
+        addForm.reset();
+      });
+    } else {
+      console.log(addForm);
+      console.error("Employee Form content is invalid");
+    }
+  }
+
+  ngOnInit(): void {
+    this.newEmployee = new Employee();
+  }
+
+}
---
+++ tractorz-app/src/app/emp-details/emp-details.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'tractorz-emp-details',
+  templateUrl: './emp-details.component.html',
+  styleUrls: ['./emp-details.component.css']
+})
+export class EmpDetailsComponent implements OnInit {
+  data: DataService;
+
+  constructor(dataService: DataService) {
+    this.data = dataService;
+  }
+
+  ngOnInit() {
+  }
+
+
+
+}
