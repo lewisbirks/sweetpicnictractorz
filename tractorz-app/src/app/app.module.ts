4f32724ecaa6ab8313255438a2bde8002899ae76
Reece Bennett <reeceb@kainos.com>
2019-08-22 10:43:13 +0100

    Fixes to Angular for working spike

--- tractorz-app/src/app/app.module.ts
+++ tractorz-app/src/app/app.module.ts
@@ -1,16 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
-import { EmployeeComponent } from './employee/employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EmployeeComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
